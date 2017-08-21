a3ae2810214ab5795131ab0d97cb0eae25db2e80
ANyanCatFan <saitam.spatz@gmail.com>
2017-08-22 00:16:48 +0200

    Adjusts time checking based on if it is running on Heroku

--- index.js
+++ index.js
@@ -162,7 +162,8 @@ setInterval(function() {
   users.once('value', usersData => {
     for (var i in usersData.val()) {
       const now = new Date();
-      if (usersData.val()[i].time == now.getHours() + ':' + now.getMinutes()) {
+      console.log(('0' + (process.env.IS_HEROKU ? (now.getHours() + 2) % 24 : now.getHours())).slice(-2) + ':' + ('0' + now.getMinutes()).slice(-2));
+      if (usersData.val()[i].time == ('0' + (process.env.IS_HEROKU ? (now.getHours() + 2) % 24 : now.getHours())).slice(-2) + ':' + ('0' + now.getMinutes()).slice(-2)) {
         sendVertretungen(usersData.val()[i].id);
       }
     }
