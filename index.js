a4fcfdc0a2ec082398767d30bd3db19a2b3abf6a
ANyanCatFan <saitam.spatz@gmail.com>
2017-08-22 00:20:22 +0200

    Removed stupid console.log()\nWHY DO I ALWAYS FORGET THIS???

--- index.js
+++ index.js
@@ -162,7 +162,6 @@ setInterval(function() {
   users.once('value', usersData => {
     for (var i in usersData.val()) {
       const now = new Date();
-      console.log(('0' + (process.env.IS_HEROKU ? (now.getHours() + 2) % 24 : now.getHours())).slice(-2) + ':' + ('0' + now.getMinutes()).slice(-2));
       if (usersData.val()[i].time == ('0' + (process.env.IS_HEROKU ? (now.getHours() + 2) % 24 : now.getHours())).slice(-2) + ':' + ('0' + now.getMinutes()).slice(-2)) {
         sendVertretungen(usersData.val()[i].id);
       }
